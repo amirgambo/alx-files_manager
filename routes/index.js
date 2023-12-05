8822552756160b0f0e0cc049abe5f89bd5f861d1
amirgambo <gambo.amir@yahoo.com>
2023-12-05 22:49:50 +0000

    Files manager

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,20 @@
 // eslint-disable-next-line no-unused-vars
 import { Express } from 'express';
 import AppController from '../controllers/AppController';
-import AuthController from '../controllers/AuthController';
 import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
+import { basicAuthenticate, xTokenAuthenticate } from '../interface/auth';
 import FilesController from '../controllers/FilesController';
-import { basicAuthenticate, xTokenAuthenticate } from '../middlewares/auth';
-import { APIError, errorResponse } from '../middlewares/error';
+import { APIError, errorResponse } from '../interface/error';
 
-/**
- * Injects routes with their handlers to the given Express application.
- * @param {Express} api
- */
 const injectRoutes = (api) => {
   api.get('/status', AppController.getStatus);
   api.get('/stats', AppController.getStats);
 
+  api.post('/users', UsersController.postNew);
+
   api.get('/connect', basicAuthenticate, AuthController.getConnect);
   api.get('/disconnect', xTokenAuthenticate, AuthController.getDisconnect);
-
-  api.post('/users', UsersController.postNew);
   api.get('/users/me', xTokenAuthenticate, UsersController.getMe);
 
   api.post('/files', xTokenAuthenticate, FilesController.postUpload);
